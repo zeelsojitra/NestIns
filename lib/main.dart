df582e8fae930f44b3e040ba260c1edbea791870
iharshSojitra <harshsojitra2004@gmail.com>
2023-04-19 23:39:37 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
         // theme: MyThemes.lightTheme,
         // darkTheme: MyThemes.darkTheme,
         themeMode: ThemeMode.system,
-        home: RoadMapScreen(),
+        home: Splash_Screen(),
         debugShowCheckedModeBanner: false,
       ),
     );
